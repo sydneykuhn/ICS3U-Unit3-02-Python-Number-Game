aaa0fd60593c2c819d5e717658be6f117837a688
Sydney <sydney.kuhn@mths.ca>
2021-09-17 16:40:13 +0000

    added number guessing game.py

---
+++ number_game.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+# Created by: Sydney Kuhn
+# Created on: Sep 2020
+# This program is a number guessing game
+
+
+def main():
+
+    # this function it the game
+
+    # input
+    guessed_number = int(input("Guess a number between 0-9: "))
+
+    # process
+    # process & output
+    if guessed_number == 5:
+        print("You guessed correctly!")
+
+    if guessed_number != 5:
+        print("You guessed wrong!")
+
+    print("")
+    print("Done.")
+
+
+if __name__ == "__main__":
+    main()
